243b4c7f0d41a8d2622c65434192f23a4f7c2225
Brandon Aldair Vera Chavez <arvera41@gmail.com>
2025-12-06 21:08:46 -0600

    Agregando variable de entorno USERDOMAIN_ROAMINGPROFILE

--- test.js
+++ test.js
@@ -8,6 +8,7 @@ const {INIT_CWD,TEMP} =  process.env;
 const DB_NAME = process.env.DB_NAME;
 const PORT = process.env.PORT;
 const DB_PWD = process.env.DB_PWD;
+const USERDOMAIN_ROAMINGPROFILE =  process.env.USERDOMAIN_ROAMINGPROFILE;
 
 // console.log(process.env.PORT ?? 8080);
 console.log(INIT_CWD);
@@ -15,3 +16,4 @@ console.log(TEMP);
 console.log(DB_NAME);
 console.log(PORT);
 console.log(DB_PWD);
+console.log(USERDOMAIN_ROAMINGPROFILE);
